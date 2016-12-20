036f46b7c47f0cb39fdcfe2ddec9edff6bc6e7bc
Fco. Fedez <Fco. Fedez@DESKTOP-ACUEFJO>
2016-12-20 18:32:41 +0100

    modificacion clase HolaMundo + Hora

--- src/tarea/HolaMundo.java
+++ src/tarea/HolaMundo.java
@@ -1,10 +1,21 @@
 package tarea;
 
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
 public class HolaMundo {
 
 	public static void main(String[] args) {
 		
 		System.out.println("Hola Mundo");
+		
+		Calendar calendario = new GregorianCalendar();
+		int hora, minutos, segundos;
+	    hora =calendario.get(Calendar.HOUR_OF_DAY);
+	    minutos = calendario.get(Calendar.MINUTE);
+	    segundos = calendario.get(Calendar.SECOND);
+	    
+	    System.out.println("son las" +hora + ":" + minutos + ":" + segundos);
 
 	}
 
